c0b7fa498b2d59383f5a729ffa92b342db297853
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-01-13 13:47:50 -0500

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'puma'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use Twitter Bootstrap
+gem 'bootstrap-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
   
-  get 'static_pages/home'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/help'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/about'
+  get '/contact', to: 'static_pages#contact'
 
-  get 'static_pages/contact'
+  get 'signup', to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
